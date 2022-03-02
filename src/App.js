828574d0756e36bedf0f2b688d9da46d6d5feb1c
yiweiShen <yiwei.shen@normcore.com>
2022-03-02 15:29:34 +0800

    feat:完成pretty组件编写

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Pretty from '@/components/Pretty'
+
+const enterData = [ 'foo', 'bar', { baz: 'baz', quz: { quz:'quz' } }, [ 'baz' ], [ 'qux' ] ]
 function App() {
   return (
     <div className="App">
-    
+      <Pretty enterData={ enterData }/>
     </div>
   );
 }
--- src/components/Pretty.js
+++ src/components/Pretty.js
@@ -1,8 +1,28 @@
+import React, { useState, useEffect } from 'react'
+import './Pretty.css';
 
-function Pretty() {
+
+function Pretty(props) {
+    
+ const { enterData } = props
+ const [prettyData, setPrettyData] = useState('')
+ const  handlePretty = ( json ) => {
+    if (typeof json != 'string') {
+        json = JSON.stringify(json, undefined, 2);
+    }
+    return json
+ }
+
+ useEffect(() => {
+    let data = handlePretty( enterData )
+    setPrettyData( data )
+ }, []);
+
   return (
-    <div className="Pretty">
-    Pretty
+    <div className="pretty" >
+        <div>输入数据：{JSON.stringify(enterData)}</div>
+        <div>输出数据:</div>
+        <pre >{ prettyData }</pre>
     </div>
   );
 }
